26171dbcb73cf00c34f8abd190d852ce777d5277
Nicolas <Nicolas@CODE-4>
2020-05-30 12:55:53 +0200

    Player -> arraylist enlever + discard

--- Hanabi/src/model/Player.java
+++ Hanabi/src/model/Player.java
@@ -4,11 +4,10 @@ public class Player {
 	
 	private String name;
 	private Card[] hand;
-	private List<Card> hand ;
 	
 	public Player(String name, SimpleGameData game, int handSize) {
 		this.name = name;
-		this.hand = Arrays.asList(new Card[handSize]);
+		this.hand = new Card[handSize];
 		for (int i = 0; i < handSize; i++) {
 			hand[i] = game.draw();
 		}
@@ -29,16 +28,7 @@ public class Player {
 		if (card_nb >= 4)
 			throw new IllegalStateException("Cette carte n'est pas dans le deck");	
 		
-		Card[] new_hand = new Card[this.handSize-1];
-		for (int i=0; i < this.handSize; i++) {
-			if(i == card_nb) {
-				i++; 
-				new_hand[i-1] = hand[i];
-			}else {
-					new_hand[i] = hand[i];
-			}
-		}
-		return ;
+		hand[nb_card] = game.draw();
 	}
 	
 	@Override
