fc58af89358224a78beeebe8c63c11eef57e5542
Nicolas <Nicolas@CODE-4>
2020-05-30 12:39:07 +0200

    Discard + arrayList card

--- Hanabi/src/model/Player.java
+++ Hanabi/src/model/Player.java
@@ -4,10 +4,11 @@ public class Player {
 	
 	private String name;
 	private Card[] hand;
+	private List<Card> hand ;
 	
 	public Player(String name, SimpleGameData game, int handSize) {
 		this.name = name;
-		this.hand = new Card[handSize];
+		this.hand = Arrays.asList(new Card[handSize]);
 		for (int i = 0; i < handSize; i++) {
 			hand[i] = game.draw();
 		}
@@ -24,8 +25,20 @@ public class Player {
 	}
 	
 	/** Défausser une carte */
-	public void discardCard() {
+	public void discardCard(int card_nb) {
+		if (card_nb >= 4)
+			throw new IllegalStateException("Cette carte n'est pas dans le deck");	
 		
+		Card[] new_hand = new Card[this.handSize-1];
+		for (int i=0; i < this.handSize; i++) {
+			if(i == card_nb) {
+				i++;
+				new_hand[i-1] = hand[i];
+			}else {
+					new_hand[i] = hand[i];
+			}
+		}
+		return ;
 	}
 	
 	@Override
